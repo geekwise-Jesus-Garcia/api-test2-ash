3f899d021e5fa2d3a24aea461c545392a3f2a0d5
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-11-27 19:03:14 +0000

    added serializers.py and added models

--- app/drf/models.py
+++ app/drf/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Branch(models.Model):
+    location_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+def __str__(self):
+    return(f"Bank {self.location_name}")
+
+#   def __str__(self):
+#     return(f" Bank Name {self.location_name} - location{self.location}")
