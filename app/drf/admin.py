b3623477720151a8600cc62ada9582204c05f75b
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-11-27 22:03:37 +0000

    models & linked them

--- app/drf/admin.py
+++ app/drf/admin.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Branch
+from .models import Branch, Customer, Account, Product
 
-# Register your models here.
+Register your models here.
 admin.site.register(Branch)
+admin.site.register(Customer)
+admin.site.register(Account)
+admin.site.register(Product)
--- app/drf/models.py
+++ app/drf/models.py
@@ -6,11 +6,55 @@ from django.db import models
 # Create your models here.
 
 class Branch(models.Model):
-    location_name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
+  location_name = models.CharField(max_length=100)
+  def __str__(self):
+        return(f" Bank {self.location_name}")
 
-def __str__(self):
-    return(f"Bank {self.location_name}")
 
-#   def __str__(self):
-#     return(f" Bank Name {self.location_name} - location{self.location}")
+class Customer(models.Model):
+    branch = models.ForeignKey(
+        Branch,
+        on_delete = models.CASCADE,
+    )
+    customer_name = models.CharField(max_length=20/0)
+    def __str__(self):
+        return(f" Name {self.customer_name}")
+
+
+class Account(models.Model):
+    branch = models.ForeignKey(
+        Branch,
+        on_delete = models.CASCADE,
+    )
+    account_options = (
+        ('account', 'ACCOUNT'),
+        ('checking', 'CHECKING'),
+        ('balance', 'BALANCE'),
+    )
+
+    product_username = models.CharField(max_length=40)
+    product_email = models.EmailField(max_length=400)
+    product_options = models.CharField(max_length=8,
+    choices=account_options,
+    default=account_options[0],
+    )
+
+    def __str__(self):
+        return(f" Account Name {self.product_username} Account Email {self.product_email}")
+
+class Product(models.Model):
+    place = models.OneToOneField(
+        Branch,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    product_options = (
+        ('loan', 'LOAN'),
+        ('credit', 'CREDIT'),
+        ('cd', 'CD'),
+    )
+    product_options = models.CharField(max_length=40)
+    product = models.EmailField(max_length=400)
+    product_options = models.CharField(max_length=8)
+
